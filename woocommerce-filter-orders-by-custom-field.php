5928d80cf8b601c61c1fd90a9643b2c717e5dc15
Lyle <lyle@webworxdesign.co.za>
2019-08-20 11:14:26 +0200

    Version 1

--- woocommerce-filter-orders-by-custom-field.php
+++ woocommerce-filter-orders-by-custom-field.php
@@ -1,17 +1,18 @@
 <?php
 /**
- * Plugin Name: WooCommerce Hide Zero Orders
- * Plugin URI: https://github.com/shopplugins/woocommerce-hide-zero-orders
- * Description: Adds a filter to the Edit Orders admin page to show all or show non-zero orders. Handy to hide $0 orders.
+ * Plugin Name: WooCommerce Filter Orders By Area
+ * Plugin URI: https://github.com/LyleBennett/woocommerce-filter-orders-by-custom-field
+ * Description: Adds a filter to the Edit Orders admin page to show orders by the custom field - billing area.
  * Version: 1.0.0
- * Author: Shop Plugins
- * Author URI: https://shopplugins.com
- * Text Domain: sp-hide-zero-orders
+ * Author: LyleBennett
+ * Author URI: https://github.com/LyleBennett/
+ * Text Domain: ww-filter-orders
  */
 
+
 if ( is_admin() ) {
-	add_action( 'restrict_manage_posts', 'sp_restrict_orders', 50 );
-	function sp_restrict_orders() {
+	add_action( 'restrict_manage_posts', 'ww_restrict_orders', 50 );
+	function ww_restrict_orders() {
 		global $typenow;
 
 		if ( 'shop_order' != $typenow ) {
@@ -19,32 +20,46 @@ if ( is_admin() ) {
 		}
 
 		?>
-		<select name='sp_order_view' id='dropdown_sp_order_view'>
+		<select name='ww_order_view' id='dropdown_ww_order_view'>
 			<option <?php
-				if ( isset( $_GET['sp_order_view'] ) && $_GET['sp_order_view'] ) {
-					selected( 'all', $_GET['sp_order_view'] );
+				if ( isset( $_GET['ww_order_view'] ) && $_GET['ww_order_view'] ) {
+					selected( 'all', $_GET['ww_order_view'] );
 				}
-			?> value="all"><?php esc_html_e( 'Show all orders', 'sp-hide-zero-orders' ); ?></option>
+			?> value="all"><?php esc_html_e( 'No Area Filter', 'ww-filter-orders' ); ?></option>
 			<option <?php
-			        if ( isset( $_GET['sp_order_view'] ) && $_GET['sp_order_view'] ) {
-				        selected( 'non-zero', $_GET['sp_order_view'] );
+			        if ( isset( $_GET['ww_order_view'] ) && $_GET['ww_order_view'] ) {
+				        selected( 'atlantic-seaboard', $_GET['ww_order_view'] );
 			        }
-			        ?>value="non-zero"><?php esc_html_e( 'Show non-zero orders', 'sp-show-zero-orders' ); ?></option>
+			        ?>value="atlantic-seaboard"><?php esc_html_e( 'Atlantic Seaboard', 'ww-filter-orders' ); ?></option>
 			<option <?php
-			        if ( isset( $_GET['sp_order_view'] ) && $_GET['sp_order_view'] ) {
-				        selected( 'all-zero', $_GET['sp_order_view'] );
+			        if ( isset( $_GET['ww_order_view'] ) && $_GET['ww_order_view'] ) {
+				        selected( 'city-bowl', $_GET['ww_order_view'] );
 			        }
-			        ?>value="all-zero"><?php esc_html_e( 'Show zero value orders', 'sp-show-zero-orders' ); ?></option>
+			        ?>value="city-bowl"><?php esc_html_e( 'City Bowl', 'ww-filter-orders' ); ?></option>
+			<option <?php
+			        if ( isset( $_GET['ww_order_view'] ) && $_GET['ww_order_view'] ) {
+				        selected( 'northern-suburbs', $_GET['ww_order_view'] );
+			        }
+			        ?>value="northern-suburbs"><?php esc_html_e( 'Northern Suburbs', 'ww-filter-orders' ); ?></option>
+			<option <?php
+			        if ( isset( $_GET['ww_order_view'] ) && $_GET['ww_order_view'] ) {
+				        selected( 'southern-suburbs', $_GET['ww_order_view'] );
+			        }
+			        ?>value="southern-suburbs"><?php esc_html_e( 'Southern Suburbs', 'ww-filter-orders' ); ?></option>
+
 		</select>
 		<?php
 	}
 
-	add_filter( 'request', 'sp_orders_by_restrict_option', 100 );
-	function sp_orders_by_restrict_option( $vars ) {
+	add_filter( 'request', 'ww_orders_by_restrict_option', 100 );
+	function ww_orders_by_restrict_option( $vars ) {
 		global $typenow;
 		$key = 'post__not_in';
-		if ( 'shop_order' == $typenow && isset( $_GET['sp_order_view'] ) ) {
-			if ( 'non-zero' == $_GET['sp_order_view'] ) {
+		if ( 'shop_order' == $typenow && isset( $_GET['ww_order_view'] ) ) {
+
+
+			//atlantic seaboard
+			if ( 'atlantic-seaboard' == $_GET['ww_order_view'] ) {
 				if ( ! empty( $key ) ) {
 					$vars[ $key ] = get_posts( array(
 						'posts_per_page' => -1,
@@ -55,18 +70,19 @@ if ( is_admin() ) {
 						'order'          => 'DESC',
 						'meta_query'     => array(
 							array(
-								'key'     => '_order_total',
-								'value'   => '0.00',
-								'compare' => '=',
+								'key'     => 'billing_area',
+								'value'   => 'Atlantic Seaboard (Mouille Point, Green Point, Sea Point, Fresnaye, Three Anchor Bay, Bantry Bay, Bakoven, Clifton, Camps Bay, Llandudno, Hout Bay)',
+								'compare' => '!=',
 							),
 						),
 					) );
 
 				}
 
-			} // if non-zero
+			}
 
-			if ( 'all-zero' == $_GET['sp_order_view'] ) {
+			//northern suburbs
+			if ( 'northern-suburbs' == $_GET['ww_order_view'] ) {
 				if ( ! empty( $key ) ) {
 					$vars[ $key ] = get_posts( array(
 						'posts_per_page' => -1,
@@ -77,8 +93,55 @@ if ( is_admin() ) {
 						'order'          => 'DESC',
 						'meta_query'     => array(
 							array(
-								'key'     => '_order_total',
-								'value'   => '0.00',
+								'key'     => 'billing_area',
+								'value'   => 'Northern Suburbs (Milnerton, Century City, Paarden Eiland, Table View, Sunset Beach, Parklands, Bellville, Bloubergstrand, Big Bay, Thorton, Plattekloof, Parow, Panorama, Monte Vista, Goodwood, Edgemead, Brooklyn, Welgemoed)',
+								'compare' => '!=',
+							),
+						),
+					) );
+
+				}
+
+			}
+
+			//city bowl
+			if ( 'city-bowl' == $_GET['ww_order_view'] ) {
+				if ( ! empty( $key ) ) {
+					$vars[ $key ] = get_posts( array(
+						'posts_per_page' => -1,
+						'post_type'      => 'shop_order',
+						'post_status'    => 'any',
+						'fields'         => 'ids',
+						'orderby'        => 'date',
+						'order'          => 'DESC',
+						'meta_query'     => array(
+							array(
+								'key'     => 'billing_area',
+								'value'   => 'City Bowl (City Bowl, Devils Peak, Higgovale, Gardens, Oranjezicht, Tamboerskloof, Vredehoek, Zonnebloem, De Waterkant, Waterfront, Woodstock, Observatory, Walmer Estate)',
+								'compare' => '!=',
+							),
+						),
+					) );
+
+				}
+
+			}
+
+			// southern suburbs
+
+			if ( 'southern-suburbs' == $_GET['ww_order_view'] ) {
+				if ( ! empty( $key ) ) {
+					$vars[ $key ] = get_posts( array(
+						'posts_per_page' => -1,
+						'post_type'      => 'shop_order',
+						'post_status'    => 'any',
+						'fields'         => 'ids',
+						'orderby'        => 'date',
+						'order'          => 'DESC',
+						'meta_query'     => array(
+							array(
+								'key'     => 'billing_area',
+								'value'   => 'Southern Suburbs (Bergvliet, Bishopscourt, Claremont, Constantia, Diep River, Kenilworth, Mowbray, Newlands, Ottery, Pinelands, Plumstead, Rondebosch, Tokai, Wynberg)',
 								'compare' => '!=',
 							),
 						),
